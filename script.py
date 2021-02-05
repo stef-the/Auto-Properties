883d81525881221b9ebe306b29f86d153b11a2ec
stef-the <75397677+stef-the@users.noreply.github.com>
2021-02-05 10:01:52 -0800

    Add "Item" and "Type" inputs + .properties file creation

--- script.py
+++ script.py
@@ -6,25 +6,35 @@ master.grid_rowconfigure(0, weight=1)
 master.grid_columnconfigure(0, weight=1)
 
 titleText = Label(master, text="CIT - Generator", font='Helvetica 15 bold')
+typeText = Label(master, text='Type')
+itemText = Label(master, text='Item')
 imageText = Label(master, text='Image [.png]')
 tk.Label(master).grid(row=2)
 tk.Label(master).grid(row=3)
 tk.Label(master).grid(row=5)
 
+typeString = tk.Entry(master)
+itemString = tk.Entry(master)
 imageString = tk.Entry(master)
 modelString = tk.Entry(master)
 loreString = tk.Entry(master)
 
 titleText.grid(row=1, column=0, sticky=W)
-imageText.grid(row=2, column=0, sticky=W)
-imageString.grid(row=2, column=1)
-modelString.grid(row=3, column=1)
-loreString.grid(row=5, column=1)
+typeText.grid(row=2, column=0, sticky=W)
+itemText.grid(row=3, column=0, sticky=W)
+imageText.grid(row=4, column=0, sticky=W)
+
+
+typeString.grid(row=2, column=1)
+itemString.grid(row=3, column=1)
+imageString.grid(row=4, column=1)
+modelString.grid(row=5, column=1)
+loreString.grid(row=6, column=1)
 
 model = IntVar()
-Checkbutton(master, text="Model [.json]", variable=model).grid(row=3, sticky=W)
+Checkbutton(master, text="Model [.json]", variable=model).grid(row=5, sticky=W)
 lore = IntVar()
-Checkbutton(master, text="Lore", variable=lore).grid(row=5, sticky=W)
+Checkbutton(master, text="Lore", variable=lore).grid(row=6, sticky=W)
 
 image_name = ''
 model_name = ''
@@ -42,6 +52,14 @@ def fetchData():
         lore_name = loreString.get()
         print(f'Lore: >{lore_name}<')
     print('data fetched')
+    print('starting file creation')
+    try:
+        propertyFile = open(f'{image_name.replace(".png", ".properties")}', 'w')
+        propertyFile.write('pog')
+        propertyFile.close()
+        print('Success')
+    except:
+        print('Error while creating file: Try again')
 
 compileBtn = tk.Button(master, text='Compile File', command=fetchData)
 compileBtn.grid(row=6, column=0, sticky=W)
