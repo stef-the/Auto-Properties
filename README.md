f842f9af8ba73835f7e54a8c650b3aea9e6b641e
stef-the <75397677+stef-the@users.noreply.github.com>
2021-02-05 10:38:06 -0800

    Update README and script
    added:
    Item Name (90%)
    Item ID (90%)
    
    Type, Item (40%)
    
    improved:
    formatting
    README.md

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # Auto-Properties
 Creates a .properties file for your Optifine CIT, based off of file input, to make texture packs a little easier to make.
 
+Started Feb. 5, 2021
+### How do I use it?
+> ...
+
+### What does it do?
+> Gives you input boxes for `type`, `item`, and `image`. Toggleable input boxes for `model`, `lore`, `item name`, and `item ID`.
+
+> Creates a `.properties` file matching image name.
+
+### Checklist
+Automatically fill in content based on inputs
+Improve UI formatting and color
+Create first release
--- script.py
+++ script.py
@@ -5,21 +5,20 @@ master = Tk()
 master.grid_rowconfigure(0, weight=1)
 master.grid_columnconfigure(0, weight=1)
 
-titleText = Label(master, text="CIT - Generator", font='Helvetica 15 bold')
+topText = Label(master, text="CIT - Generator", font='Helvetica 15 bold')
 typeText = Label(master, text='Type')
 itemText = Label(master, text='Item')
 imageText = Label(master, text='Image [.png]')
-tk.Label(master).grid(row=2)
-tk.Label(master).grid(row=3)
-tk.Label(master).grid(row=5)
 
 typeString = tk.Entry(master)
 itemString = tk.Entry(master)
 imageString = tk.Entry(master)
 modelString = tk.Entry(master)
 loreString = tk.Entry(master)
+itemTitleString = tk.Entry(master)
+identString = tk.Entry(master)
 
-titleText.grid(row=1, column=0, sticky=W)
+topText.grid(row=1, column=0, sticky=W)
 typeText.grid(row=2, column=0, sticky=W)
 itemText.grid(row=3, column=0, sticky=W)
 imageText.grid(row=4, column=0, sticky=W)
@@ -30,11 +29,17 @@ itemString.grid(row=3, column=1)
 imageString.grid(row=4, column=1)
 modelString.grid(row=5, column=1)
 loreString.grid(row=6, column=1)
+itemTitleString.grid(row=7, column=1)
+identString.grid(row=8, column=1)
 
 model = IntVar()
 Checkbutton(master, text="Model [.json]", variable=model).grid(row=5, sticky=W)
 lore = IntVar()
 Checkbutton(master, text="Lore", variable=lore).grid(row=6, sticky=W)
+itemTitle = IntVar()
+Checkbutton(master, text="Item Name", variable=itemTitle).grid(row=7, sticky=W)
+ident = IntVar()
+Checkbutton(master, text="Item ID", variable=ident).grid(row=8, sticky=W)
 
 image_name = ''
 model_name = ''
@@ -45,23 +50,41 @@ def fetchData():
     print('fetching data...')
     image_name = imageString.get()
     print(f'Image Name: >{image_name}<')
+
     if model.get() == 1:
         model_name = modelString.get()
         print(f'Model Name: >{model_name}<')
-    if lore.get == 1:
+
+    elif lore.get() == 1:
         lore_name = loreString.get()
         print(f'Lore: >{lore_name}<')
+
+    elif itemTitle.get() == 1:
+        item_name = itemTitleString.get()
+        print(f'Item Title: >{item_name}<')
+
+    elif ident.get() == 1:
+        item_id = identString.get()
+        print(f'Item ID: >{item_id}<')
+
     print('data fetched')
-    print('starting file creation')
+    print('Starting file creation...')
+    
     try:
-        propertyFile = open(f'{image_name.replace(".png", ".properties")}', 'w')
+        if '.png' in image_name:
+            propertyFile = open(f'{image_name.replace(".png", ".properties")}', 'w')
+            print('Created without ".png"')
+        else:
+            propertyFile = open(f'{image_name}.properties', 'w')
+            print('Created')
+            
         propertyFile.write('pog')
         propertyFile.close()
         print('Success')
     except:
-        print('Error while creating file: Try again')
+        print('Error while creating file, look for:\n- blank name\n- duplicate file')
 
-compileBtn = tk.Button(master, text='Compile File', command=fetchData)
-compileBtn.grid(row=6, column=0, sticky=W)
+compileBtn = tk.Button(master, text='Create File', command=fetchData)
+compileBtn.grid(row=9, column=0, sticky=W)
 
 master.mainloop()
