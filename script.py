27f592725cd587007aae74a7b09afdafb8480cd6
stef-the <75397677+stef-the@users.noreply.github.com>
2021-02-05 09:47:32 -0800

    Update to fetch information
    Fetches information based on Checkmarks and a "compile" button (WIP)

--- script.py
+++ script.py
@@ -5,6 +5,7 @@ master = Tk()
 master.grid_rowconfigure(0, weight=1)
 master.grid_columnconfigure(0, weight=1)
 
+titleText = Label(master, text="CIT - Generator", font='Helvetica 15 bold')
 imageText = Label(master, text='Image [.png]')
 tk.Label(master).grid(row=2)
 tk.Label(master).grid(row=3)
@@ -14,7 +15,8 @@ imageString = tk.Entry(master)
 modelString = tk.Entry(master)
 loreString = tk.Entry(master)
 
-imageText.grid(row=2, column=0)
+titleText.grid(row=1, column=0, sticky=W)
+imageText.grid(row=2, column=0, sticky=W)
 imageString.grid(row=2, column=1)
 modelString.grid(row=3, column=1)
 loreString.grid(row=5, column=1)
@@ -24,8 +26,24 @@ Checkbutton(master, text="Model [.json]", variable=model).grid(row=3, sticky=W)
 lore = IntVar()
 Checkbutton(master, text="Lore", variable=lore).grid(row=5, sticky=W)
 
+image_name = ''
+model_name = ''
+lore_name = ''
+
+
 def fetchData():
-    print('fetching data')
-runBtn = tk.Button(master, text='pog', command = fetchData())
+    print('fetching data...')
+    image_name = imageString.get()
+    print(f'Image Name: >{image_name}<')
+    if model.get() == 1:
+        model_name = modelString.get()
+        print(f'Model Name: >{model_name}<')
+    if lore.get == 1:
+        lore_name = loreString.get()
+        print(f'Lore: >{lore_name}<')
+    print('data fetched')
+
+compileBtn = tk.Button(master, text='Compile File', command=fetchData)
+compileBtn.grid(row=6, column=0, sticky=W)
 
 master.mainloop()
