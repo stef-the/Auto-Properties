67d2c45f568648f0d5b7517446a0f84a3e752827
stef-the <75397677+stef-the@users.noreply.github.com>
2021-02-08 09:16:47 -0800

    make main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter import *
+import requests
+
+url1 = 'https://raw.githubusercontent.com/stef-the/Auto-Properties/master/main/p.script/item.p.py'
+itemSc = requests.get(url1)
+url2 = 'https://raw.githubusercontent.com/stef-the/Auto-Properties/master/main/p.script/bow.p.py'
+bowSc = requests.get(url2)
+url3 = 'https://raw.githubusercontent.com/stef-the/Auto-Properties/master/main/p.script/rod.p.py'
+rodSc = requests.get(url3)
+
+def runItem():
+    exec(itemSc.text)
+def runBow():
+    exec(bowSc.text)
+def runRod():
+    exec(rodSc.text)
+
+master = Tk()
+master.title('select mode')
+master.grid_rowconfigure(0, weight=1)
+master.grid_columnconfigure(0, weight=1)
+
+itemBtn = tk.Button(master, text='Item', command=runItem)
+itemBtn.grid(row=1, column=1, sticky=W)
+bowBtn = tk.Button(master, text='Bow', command=runBow)
+bowBtn.grid(row=1, column=2, sticky=W)
+rodBtn = tk.Button(master, text='Rod', command=runRod)
+rodBtn.grid(row=1, column=3, sticky=W)
+
+master.mainloop()
